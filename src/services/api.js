7787a595b1ad357be871eb17dd6af5e943674362
Keys HD <keys208@hotmail.com>
2021-11-06 07:15:33 -0300

    ✨ Fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,43 +1,53 @@
 export function imageInput(base64) {
   return new Promise((res, rej) => {
+    const fetchheaders = new Headers();
+    fetchheaders.append('Content-type', 'application/json');
+
     const options = {
       method: 'POST',
+      headers: fetchheaders,
       mode: 'cors',
-      body: JSON.stringify({code: base64}),
+      body: {code: base64},
     };
 
-    fetch('https://sudoku-solver-fast.herokuapp.com/image', options)
-      .then(async (response) => {
+    fetch('https://sudoku-solver-fast.herokuapp.com/image', options).then(
+      async (response) => {
         const content = await response.json();
-        if(content.board) {
+        if (content.board) {
           return res(content);
         } else if (content.detail) {
           return rej(content.detail);
         } else {
           return rej('Server error');
         }
-      })
-  })
+      }
+    );
+  });
 }
 
 export function solveInput(grid) {
   return new Promise((res, rej) => {
+    const fetchheaders = new Headers();
+    fetchheaders.append('Content-type', 'application/json');
+
     const options = {
       method: 'POST',
+      headers: fetchheaders,
       mode: 'cors',
-      body: JSON.stringify({table: grid}),
+      body: {table: grid},
     };
 
-    fetch('https://sudoku-solver-fast.herokuapp.com/solve', options)
-      .then(async (response) => {
+    fetch('https://sudoku-solver-fast.herokuapp.com/solve', options).then(
+      async (response) => {
         const content = await response.json();
-        if(content.board) {
+        if (content.board) {
           return res(content.board);
         } else if (content.detail) {
-          return rej(content.detail)
+          return rej(content.detail);
         } else {
           return rej('Server error');
         }
-      })
+      }
+    );
   });
 }
